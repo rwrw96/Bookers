2a17f689919319cbc4d24d2bf573950461b9a6b5
EC2 Default User <ec2-user@ip-172-31-21-35.ec2.internal>
2021-02-03 23:17:17 +0000

    新規登録画面の追加

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -3,9 +3,17 @@ class BookersController < ApplicationController
   end
 
   def show
+    @bookers = Booker.find(params[:id])
   end
 
   def new
+    @bookers = Booker.new
+  end
+
+  def create
+    bookers = Booker.new(params[:id])
+    bookers.save
+    redirect_to "/"
   end
 
   def edit
